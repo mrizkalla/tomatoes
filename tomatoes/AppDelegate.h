17fb48e3a31d0b225e86c4a444350c55069e8f70
Michael Rizkalla <rizkalla@yahoo-inc.com>
2014-01-07 17:42:26 -0800

    Initial Commit

---
+++ tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tomatoes
+//
+//  Created by Michael Rizkalla on 1/7/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tomatoes
+//
+//  Created by Michael Rizkalla on 1/7/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
